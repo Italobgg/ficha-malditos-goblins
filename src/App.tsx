57461d18c9aceff3172ecbca69f640bf159b1b70
Italobgg <italobartolomeugarcia@gmail.com>
2025-07-23 23:16:23 -0300

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FichasGoblinsManager from "./components/FichasGoblinsManager";
 
 export default function App() {
   return (
-    <main className="bg-lime-950 min-h-screen py-10 px-4 text-lime-100">
+    <main className="bg-zinc-950 min-h-screen py-10 px-4 text-lime-100">
       <div className="max-w-3xl mx-auto">
         <FichasGoblinsManager />
       </div>
